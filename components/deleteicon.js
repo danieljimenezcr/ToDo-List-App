ef4207a832d98e86411efb530db64c16a75bf926
Daniel Jimenez <danieljimenezch@Daniels-MacBook-Pro.local>
2022-11-18 11:56:04 -0600

    Actualizacion en codigos de componentes en Add Task y Delete iCon

--- components/deleteicon.js
+++ components/deleteicon.js
@@ -1,13 +1,13 @@
 const deleteIcon = () => {
-  const i = document.createElement("i");
-  i.classList.add("fa-solid", "fa-trash-can", "trashIcon");
-  i.addEventListener("click", deleteTask);
+  const i = document.createElement('i');
+  i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+  i.addEventListener('click', deleteTask);
   return i;
 };
 
 const deleteTask = (event) => {
   const parent = event.target.parentElement;
-  console.log(parent);
   parent.remove();
 };
+
 export default deleteIcon;
