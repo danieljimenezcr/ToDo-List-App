99db0be2e3dd675444239bd1ef9f6d78b8ea9e69
Daniel Jimenez <danieljimenezch@Daniels-MacBook-Pro.local>
2022-11-18 11:35:45 -0600

    Add new component to add Task

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ import deleteIcon from "./components/deleteicon.js";
 const btn = document.querySelector("[data-form-btn]");
 const value = "";
 
+const addTask = (event) => {
+  const list = document.querySelector("[data-list]");
+  const task = createTask(event)
+  list.appendChild(task);
+};
+
+const taskList = [];
+
 const createTask = (event) => {
   event.preventDefault();
   const input = document.querySelector("[data-form-input]");
@@ -12,19 +20,34 @@ const createTask = (event) => {
   const dateFormat = moment(date).format('DD/MM/YYYY');
   const value = input.value;
   const task = document.createElement("li");
-  const list = document.querySelector("[data-list]");
+
   task.classList.add("card");
   input.value = "";
   const taskContent = document.createElement("div");
+
+  const taskObj = {
+    value,
+    dateFormat,
+  }
+
+  taskList.push(taskObj)
+
+  localStorage.setItem('task', JSON.stringify(taskList));
+
+
   taskContent.appendChild(checkComplete());
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
   taskContent.appendChild(titleTask);
 
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
+
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
